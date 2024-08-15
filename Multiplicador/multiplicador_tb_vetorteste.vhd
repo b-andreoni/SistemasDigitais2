107d0f0cd753d0892451e7e57d3f4dd7db3b92ba
Enzo-Takao <147572343+Enzo-Takao@users.noreply.github.com>
2024-08-14 23:49:31 -0300

    Update multiplicador_tb_vetorteste.vhd
    Versão Final

--- Multiplicador/multiplicador_tb_vetorteste.vhd
+++ Multiplicador/multiplicador_tb_vetorteste.vhd
@@ -8,10 +8,10 @@
 library ieee;
 use ieee.numeric_bit.all;
 
-entity multiplicador_tb_vetor is
+entity multiplicador_tb is
 end entity;
 
-architecture tb of multiplicador_tb_vetor is
+architecture tb of multiplicador_tb is
   
   -- Componente a ser testado (Device Under Test -- DUT)
   component multiplicador
@@ -35,63 +35,78 @@ architecture tb of multiplicador_tb_vetor is
   signal keep_simulating: bit := '0'; -- delimita o tempo de geração do clock
   constant clockPeriod : time := 1 ns;
   
-  -- Definição do tipo pattern_type e do vetor de padrões de teste
-  type pattern_type is record
-    -- Entradas
-    op1: bit_vector(3 downto 0);
-    op2: bit_vector(3 downto 0);
-    -- Saídas esperadas
-    mult_esperada: bit_vector(7 downto 0);
-  end record;
-
-  type pattern_array is array (natural range <>) of pattern_type;
-
-  constant patterns: pattern_array := (
-    --  op1    op2     resultado esperado
-    ("0011", "0110", "00010010"),    -- 3 x 6  = 18    
-    ("1111", "1011", "10100101"),    -- 15 x 11 = 165
-    ("1111", "0000", "00000000"),    -- 15 x 0  = 0
-    ("0001", "1011", "00001011")     -- 1 x 11  = 11
-  );
-
 begin
   -- Gerador de clock: executa enquanto 'keep_simulating = 1', com o período
   -- especificado. Quando keep_simulating=0, clock é interrompido, bem como a 
   -- simulação de eventos
   clk_in <= (not clk_in) and keep_simulating after clockPeriod/2;
   
+  ---- O código abaixo, sem o "keep_simulating", faria com que o clock executasse
+  ---- indefinidamente, de modo que a simulação teria que ser interrompida manualmente
+  -- clk_in <= (not clk_in) after clockPeriod/2; 
+  
   -- Conecta DUT (Device Under Test)
   dut: multiplicador
-    port map(
-      Clock => clk_in,
-      Reset => rst_in,
-      Start => start_in,
-      Va => va_in,
-      Vb => vb_in,
-      Vresult => result_out,
-      Ready => ready_out
-    );
+       port map (Clock=>   clk_in,
+                Reset=>   rst_in,
+                Start=>   start_in,
+                Va=>      va_in,
+                Vb=>      vb_in,
+                Vresult=> result_out,
+                Ready=>   ready_out
+      );
 
-  -- Gera sinais de estímulo
-  stimulus: process
+  ---- Gera sinais de estimulo
+  stimulus: process is
+    
+    type pattern_type is record
+        -- Entradas
+        op1: bit_vector(3 downto 0);
+        op2: bit_vector(3 downto 0);
+        -- Saídas
+        mult_esperada: bit_vector(7 downto 0);
+  	end record;
+
+  	type pattern_array is array (natural range<>) of pattern_type;
+
+  constant patterns: pattern_array :=
+  --  op1    op2     resultado          op1   op2   resultado
+  (("0011", "0110", "00010010"),    --    3 x 6   =   18    
+  ("1111","1011","10100101"),       --   15 x 11  =   165
+  ("1111","0000","00000000"),       --   15 x 0   =   0
+  ("0001","1011","00001011")       --    1 x 11  =   11
+  );
   begin
-    -- Para cada padrão de teste no vetor
-    for i in patterns'range loop
-      -- Injeta as entradas
-      va_in <= patterns(i).op1;
-      vb_in <= patterns(i).op2;
-      -- Aguarda que o módulo produza a saída
-      wait for 10 ns;
-      -- Verifica as saídas
-      assert result_out = patterns(i).mult_esperada
-        report "Erro na multiplicação " & integer'image(to_integer(unsigned(patterns(i).op1))) & " * " & integer'image(to_integer(unsigned(patterns(i).op2)))
-        severity error;
-    end loop;
-
-    -- Informa fim do teste
-    assert false report "Teste concluído." severity note;	  
-    wait;  -- pára a execução do simulador, caso contrário este processo é reexecutado indefinidamente.
-  end process;
-
-end architecture;
+      keep_simulating <= '1';
+      
+	  -- Para cada padrao de teste no vetor
+      for i in patterns'range loop
+         -- Injeta as entradas
+         va_in <= patterns(i).op1;
+		 vb_in <= patterns(i).op2;
+         
+         -- Reset inicial (1 periodo de clock)
+          rst_in <= '1'; start_in <= '0';
+          wait for clockPeriod;
+          rst_in <= '0';
+          wait until falling_edge(clk_in);
+          -- Pulso do sinal de Start
+          start_in <= '1';
+          wait until falling_edge(clk_in);
+          start_in <= '0';
+          -- Espera pelo termino da multiplicacao
+          wait until ready_out='1';
+         
+         --  Verifica as saidas
+         assert result_out = patterns(i).mult_esperada report "Erro na multiplicação " & integer'image(to_integer(unsigned(patterns(i).op1))) & " * " & 
+         integer'image(to_integer(unsigned(patterns(i).op2))) & " (Deu: " & integer'image(to_integer(unsigned(result_out))) & ") " & "Era pra ser: " & 
+         integer'image(to_integer(unsigned(patterns(i).mult_esperada)))  severity error;
+         
+      	wait for clockPeriod;
+      end loop;
+	  -- Informa fim do teste
+	  assert false report "Teste concluido." severity note;	 
+      keep_simulating <= '0';
+	  wait;  -- pára a execução do simulador, caso contrário este process é reexecutado indefinidamente.
+   end process;
 
