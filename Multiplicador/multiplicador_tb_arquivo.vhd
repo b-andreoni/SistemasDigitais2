658c2a170f7a481af4db5a1dd1242bac5ce41100
Vinicius Gomes da Silva <v.gomes@usp.br>
2024-08-14 23:10:17 -0300

    65 = 81

--- Multiplicador/multiplicador_tb_arquivo.vhd
+++ Multiplicador/multiplicador_tb_arquivo.vhd
@@ -7,23 +7,23 @@ end multiplicador_tb_arquivo;
 
 architecture Multiplicador_Read of multiplicador_tb_arquivo is
 
-   -- Declara o componente do DUT
-   component multiplicador 
-    port (
-       Clock:    in  bit;
-       Reset:    in  bit;
-       Start:    in  bit;
-       Va, Vb:   in  bit_vector(3 downto 0);
-       Vresult:  out bit_vector(7 downto 0);
-       Ready:    out bit
-    );
-   end component;
+    -- Declara o componente do DUT
+    component multiplicador 
+        port (
+            Clock:    in  bit;
+            Reset:    in  bit;
+            Start:    in  bit;
+            Va, Vb:   in  bit_vector(3 downto 0);
+            Vresult:  out bit_vector(7 downto 0);
+            Ready:    out bit
+        );
+    end component;
 
-   -- Sinais de teste
-   signal clk_in: bit := '0';
-   signal rst_in, start_in, ready_out: bit := '0';
-   signal va_in, vb_in: bit_vector(3 downto 0);
-   signal result_out: bit_vector(7 downto 0);
+    -- Sinais de teste
+    signal clk_in: bit := '0';
+    signal rst_in, start_in, ready_out: bit := '0';
+    signal va_in, vb_in: bit_vector(3 downto 0);
+    signal result_out: bit_vector(7 downto 0);
 
 begin
 
@@ -38,6 +38,17 @@ begin
         Vresult => result_out,
         Ready => ready_out
     );
+
+    -- Geração do clock
+    clk_gen: process
+    begin
+        while true loop
+            clk_in <= '0';
+            wait for 5 ns;
+            clk_in <= '1';
+            wait for 5 ns;
+        end loop;
+    end process clk_gen;
 
     -- Processo de geração de estímulos
     gerador_estimulos: process
@@ -63,6 +74,14 @@ begin
             va_in <= VaRead;
             vb_in <= VbRead; 
             wait for 10 ns;
+
+            -- Ativa o start para iniciar a multiplicação
+            start_in <= '1';
+            wait for 10 ns;
+            start_in <= '0';
+
+            -- Espera até que o sinal Ready esteja ativo
+            wait until ready_out = '1';
 
             assert result_out = result_out_read
                 report "Multiplicacao " & integer'image(to_integer(unsigned(VaRead))) & " * " & integer'image(to_integer(unsigned(VbRead))) & " falhou: Esperado " & integer'image(to_integer(unsigned(result_out_read))) & ", obtido " & integer'image(to_integer(unsigned(result_out))) severity error;
