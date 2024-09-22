7ae9cf9e947ca2a24e7ece17a9778c41e5f91a78
Vinicius Gomes da Silva <143216879+v-gomes28@users.noreply.github.com>
2024-09-21 22:44:46 -0300

    Mudei bastante coisa

--- Exercicio 18.09/ContaUm_uc.vhdl
+++ Exercicio 18.09/ContaUm_uc.vhdl
@@ -1,27 +1,30 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
 entity onescounter_uc is
-port (
-clock : in bit ;
-reset : in bit ;
-start : in bit ;
-data0 : in bit ;
-zero : in bit ;
-zera : out bit ;
-conta : out bit;
-carrega : out bit;
-desloca : out bit;
-done : out bit;
-) ;
+    port (
+        clock : in bit;
+        reset : in bit;
+        start : in bit;
+        data0 : in bit;
+        zero : in bit;
+        zera : out bit;
+        conta : out bit;
+        carrega : out bit;
+        desloca : out bit;
+        done : out bit
+    );
 end entity;
 
 architecture fsm of onescounter_uc is
     type estado_t is (S0, S1, S2, S3, S4, S5);
     signal PE, EA: estado_t;
 begin
-    sincronoprocess: process(clock, reset, PE)
+    sincronoprocess: process(clock, reset)
     begin
         if (reset = '1') then
             EA <= S0;
-        elsif (rising_edge(clock)) then
+        elsif (clock = '1') and (clock'event) then
             EA <= PE;
         end if;
     end process sincronoprocess;
@@ -40,7 +43,6 @@ begin
                 conta <= '0';
                 carrega <= '0';
                 desloca <= '0';
-                registra <= '0';
             when S1 =>
                 PE <= S2;
                 done <= '0';
@@ -48,33 +50,30 @@ begin
                 carrega <= '1';
                 conta <= '0';
                 desloca <= '0';
-                registra <= '0';
             when S2 =>
                 if data0 = '1' then
                     PE <= S3;
                 else
                     PE <= S4;
                 end if;
-            When S3 => 
+            when S3 => 
                 PE <= S4;
                 done <= '0';
                 zera <= '0';
                 carrega <= '0';
                 conta <= '1';
                 desloca <= '0';
-                registra <= '0';
-            When S4 => 
-            if Zero = '1' then
-                PE <= S5;
-            else
-                PE <= S2;
-            end if;
+            when S4 => 
+                if zero = '1' then
+                    PE <= S5;
+                else
+                    PE <= S2;
+                end if;
                 done <= '0';
                 zera <= '0';
                 carrega <= '0';
                 conta <= '0';
                 desloca <= '1';
-                registra <= '0';  
             when S5 =>
                 PE <= S0; 
         end case;
--- Exercicio 18.09/Contador.vhdl
+++ Exercicio 18.09/Contador.vhdl
@@ -1,22 +1,57 @@
-library ieee;
+library IEEE;
+use IEEE.std_logic_1164.all;
 
 entity contador4 is
-port (
-clock: in bit ;
-zera : in bit ;
-conta : in bit ;
-Q : out bit_vector ( 3 downto 0 ) ;
-fim : out bit
-) ;
-end entity contador4 ;
+    port (
+        clock : in bit;
+        zera : in bit;
+        conta : in bit;
+        Q : out bit_vector (3 downto 0);
+        fim : out bit
+    );
+end entity contador4;
 
 architecture counter of contador4 is
-    signal 
-
-    if zera == 1 then
-        Q <= "0000";
-    else if 
-        Q <= bit_vector(Q + '1') ;
+    signal Q0, Q1, Q2, Q3 : bit := '0';  -- Sinais para os bits do contador
+    signal clock_prev : bit := '0';      -- Sinal para armazenar o estado anterior do clock
 begin
+    process(clock, zera)
+    begin
+        if (zera = '1') then
+            Q0 <= '0';
+            Q1 <= '0';
+            Q2 <= '0';
+            Q3 <= '0';  -- Reseta o contador
+        elsif (clock = '1' and clock_prev = '0') then
+            if (conta = '1') then
+                -- Incrementa manualmente
+                if (Q3 = '0' and Q2 = '0' and Q1 = '0' and Q0 = '0') then
+                    Q0 <= '1';
+                elsif (Q3 = '0' and Q2 = '0' and Q1 = '0' and Q0 = '1') then
+                    Q0 <= '0'; Q1 <= '1';
+                elsif (Q3 = '0' and Q2 = '0' and Q1 = '1' and Q0 = '0') then
+                    Q1 <= '0'; Q2 <= '1';
+                elsif (Q3 = '0' and Q2 = '1' and Q1 = '1' and Q0 = '0') then
+                    Q2 <= '0'; Q3 <= '1';
+                elsif (Q3 = '1' and Q2 = '1' and Q1 = '1' and Q0 = '1') then
+                    Q0 <= '0'; Q1 <= '0'; Q2 <= '0'; Q3 <= '0';  -- Volta a zero após o máximo
+                else
+                    -- Lógica de incremento
+                    if (Q0 = '1') then
+                        Q0 <= '0';
+                    elsif (Q1 = '1') then
+                        Q1 <= '0'; Q0 <= '1';
+                    elsif (Q2 = '1') then
+                        Q2 <= '0'; Q1 <= '1';
+                    elsif (Q3 = '1') then
+                        Q3 <= '0'; Q2 <= '1';
+                    end if;
+                end if;
+            end if;
+        end if;
+        clock_prev <= clock;  -- Atualiza o estado anterior do clock
+    end process;
 
+    Q <= Q3 & Q2 & Q1 & Q0;  -- Concatena os bits em Q
+    fim <= '0';  -- Defina o sinal 'fim' conforme necessário
 end architecture;
