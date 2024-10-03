8219a1b5bb60919c0ffeb8804b5379b39695c677
Fernando <145460300+Ecronite@users.noreply.github.com>
2024-10-02 21:12:57 -0300

    Add files via upload

--- Atividade 6/Testbench_log.vhdl
+++ Atividade 6/Testbench_log.vhdl
@@ -7,8 +7,6 @@ end testbench;
 -- Complete no espaço os sinais e componentes a serem utilizados
 
 architecture beh of testbench is
-    constant clockPeriod : time := 10 ns;
-    
     component log 
     port (
         clock, inicio : in bit;
@@ -18,6 +16,7 @@ architecture beh of testbench is
     );
    end component;
 
+   Signal clk : bit
    signal clk_in, inicio_in : bit;
    signal x_in: bit_vector(7 downto 0);
    signal R_in: bit_vector(7 downto 0);
@@ -26,8 +25,7 @@ architecture beh of testbench is
     -- Depois, complete no espaço para que o DUT seja instanciado
 
 begin
-    clk <= '1' after 10 ns;
-    clock <= not clock and keep after clockPeriod/2; -- não sei calcular clock
+    clk <= not clk and keep after 10 ns;
 
     DUT: multiplicador
     port map (
@@ -40,16 +38,18 @@ begin
 
     process
     begin
-        x <= "11110000";
+        run <= '1'; -- não sei onde aplicar esses módulos de run
+
+        x <= "11110000"; -- indicando a entrada do circuito log
         inicio <= '1';  
-        wait for clk_period;  
+        wait for 10 ns ;  
         inicio <= '0';
         wait until fim = '1';
+        
         Assert R = 01001010
             Report "Teste 1 - incorreto" severity error;
-        
         Assert false report "Teste concluido" severity note;
-        run <= '1'; -- não sei onde aplicar esses módulos de run
+        
         run <= '0';
         wait;
     end process;
